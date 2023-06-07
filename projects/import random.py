4fbe03541f33ae4de9d90830bdcc41bac4c404e9
Lina Franco <131540750+linafranco1@users.noreply.github.com>
2023-06-07 16:42:15 -0500

    Upload folder

---
+++ projects/import random.py
@@ -0,0 +1,10 @@
+import random
+adverbs=["Lately", "Today", "Now"]
+subjets=["Lina", "Nicolas", "Andres"]
+verbs=["plays tennis", "reads books", "eats burgers"]
+sustantivs= ["in Bogota", "in the club", "in the park"]
+choice1=random.choice(adverbs)
+choice2=random.choice(subjets) 
+choice3=random.choice(verbs)
+choice4=random.choice(sustantivs)
+print(choice1, choice2, choice3, choice4)
